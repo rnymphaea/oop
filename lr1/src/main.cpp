b7b0366174d7464e0d3662a16ddd99c3196b4048
Lvov Alexandr <strangeisdoctor@gmail.com>
2024-10-11 16:19:00 +0300

    lab1 finished

--- lr1/src/main.cpp
+++ lr1/src/main.cpp
@@ -10,18 +10,19 @@ int main() {
         Field field;
         auto ships = manager.getShips();
         try {
-            field.placeShip({1, 30}, ships[0], Orientation::Horizontal);
+            field.placeShip({1, 2}, ships[0], Orientation::Horizontal);
             field.placeShip({3, 4}, ships[1]);
             field.placeShip({6, 8}, ships[2], Orientation::Horizontal);
             field.placeShip({1, 7}, ships[3]);
+            field.setVisibility();
             field.printField();
         }
         catch (std::invalid_argument&) {
             std::cout << "Cannot place this segments!" << std::endl;
         }
-        field.attack({2, 2});
-        field.attack({1, 1});
-        field.attack({2, 1});
+        field.attack({3, 4});
+        field.attack({1, 3});
+        field.attack({1, 3});
         field.printField();
     }
     catch (std::invalid_argument&) {
